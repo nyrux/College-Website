46239be658c9b05428ff9799297f93acd8dc4aea
niram <nirambrt3@gmail.com>
2024-10-19 08:49:39 +0545

    ReDesign Landing Section & Navbar

--- utilities/navbar/navbar.js
+++ utilities/navbar/navbar.js
@@ -1,18 +1,35 @@
 const navBars = document.querySelector('.fa-bars');
-const slidingNav = document.querySelector('.sliding-nav');
-const crossBtn = document.querySelector('.fa-x');
+const nav = document.querySelector('nav');
+const dropDownNav = document.querySelector('.nav-dropdown');
 
-navBars.addEventListener('click', () => {
-    slidingNav.style.display = 'block';
-    setTimeout(() => {
-        slidingNav.style.left = '50vw';
-    }, 10);
-});
 
-crossBtn.addEventListener('click', () => {
-    slidingNav.style.left = '100vw';
-    setTimeout(() => {
-        slidingNav.style.display = 'none'; 
-    }, 500);
-});
+const initialNavHeight = nav.style.height;
 
+navBars.addEventListener('click', toggleNav);
+
+function toggleNav() {
+    if (navBars.classList.contains('fa-bars')) {
+        navBars.classList.remove('fa-bars');
+        navBars.classList.add('fa-x');
+        nav.style.height = '100vh';
+        dropDownNav.style.display = 'grid';
+        
+        setTimeout(() => {
+            dropDownNav.style.opacity = 1;
+        }, 200);
+    } else {
+        navBars.classList.remove('fa-x');
+        navBars.classList.add('fa-bars');
+        nav.style.height = initialNavHeight; 
+        dropDownNav.style.opacity = 0;
+        setTimeout(() => {
+            dropDownNav.style.display = 'none';
+        }, 300);
+    }
+}
+
+
+if (navigator.userAgent.includes("Chrome")) {
+    const nav = document.querySelector('nav');
+    nav.style.backdropFilter = 'blur(5px)';
+}
