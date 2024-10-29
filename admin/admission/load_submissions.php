4cb6c97981d4834d3e72d2b5a8ef76d9de38236c
niram <nirambrt3@gmail.com>
2024-10-29 06:44:02 +0545

    Add login type option

--- admin/admission/load_submissions.php
+++ admin/admission/load_submissions.php
@@ -1,5 +1,5 @@
 <?php
-include "../../visitor/php/config.php";
+include "../../php/config.php";
 $sql = "SELECT id, first_name, last_name, email, phone_number, province, course, dob, grade, image, submitted_at FROM admissions_data WHERE status IS NULL";
 $result = $conn->query($sql);
 
